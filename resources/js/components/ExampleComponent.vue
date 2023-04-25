30995613cc06cb2b01b3c62ac92d5c326aac4165
lo duy hoan <duyhoan3721@gmail.com>
2023-04-25 13:55:20 +0700

    secon commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2 class="text-red-600 text-xl ">ok</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
